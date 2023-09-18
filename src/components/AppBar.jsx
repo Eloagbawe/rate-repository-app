1b90b00df920d1631a750f27ecd9b682979302f5
Eloagbawe <eloagbawe@gmail.com>
2023-09-18 23:51:36 +0100

    add theme and update appbar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,25 +1,51 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 import Constants from 'expo-constants';
+import theme from '../theme'
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#25292B',
-    paddingBottom: 15
+    backgroundColor: theme.colors.bgAppBar,
+    paddingBottom: 15,
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: 3
 
     // ...
   },
+  flexItem: {
+    flexGrow: 0,
+    width: '30%',
+  },
   text: {
    color: '#fff',
-   fontWeight: 'bold'
+   fontWeight: 'bold',
+
   }
   // ...
 });
 
+const AppBarTab = (props) => {
+  const {tab} = props;
+  const handlePress = () => {
+    console.log('pressed')
+  }
+
+  return (
+    <View style={styles.flexItem}>
+      <Pressable onPress={handlePress}>
+        <Text style={ styles.text}>{tab}</Text>
+      </Pressable>
+    </View>
+    )
+}
+
 const AppBar = () => {
-  return <View style={styles.container}>
-   <Text style={styles.text}>Repositories</Text>
-  </View>;
+
+  return (
+    <View style={styles.container}>
+      <AppBarTab tab={'Repositories'}/>
+    </View>)
 };
 
 export default AppBar;
