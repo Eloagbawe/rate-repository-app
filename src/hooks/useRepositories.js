01d97dd38296c9e47f8822bcc20836f039b29b2c
Eloagbawe <eloagbawe@gmail.com>
2023-09-28 14:47:24 +0100

    add graphql queries file and custom hook to fetch repositories

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,30 @@
+// import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+    // Other options
+  });
+  // const [loading, setLoading] = useState(false);
+
+  // const fetchRepositories = async () => {
+  //   setLoading(true);
+
+  //   // Replace the IP address part with your own IP address!
+  //   const response = await fetch('http://192.168.100.25:5005/api/repositories');
+  //   const json = await response.json();
+
+  //   setLoading(false);
+  //   setRepositories(json);
+  // };
+
+  // return { repositories: data, loading, refetch: fetchRepositories };
+  return { repositories: data?.repositories, error, loading };
+
+ 
+};
+
+export default useRepositories;
