70082d29fc2faf56f8a275dd49c086693fce1c0b
Eloagbawe <eloagbawe@gmail.com>
2023-09-30 00:39:02 +0100

    fully implement user authentication

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,14 @@
 import { View, StyleSheet, Pressable,  ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Text from './Text';
-
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../graphql/queries';
 import Constants from 'expo-constants';
 import theme from '../theme'
+import useAuthStorage  from '../hooks/useAuthStorage';
+import { useApolloClient } from '@apollo/client';
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -36,29 +41,44 @@ const styles = StyleSheet.create({
 });
 
 const AppBarTab = (props) => {
-  const { tab, route } = props;
+
+  const { tab, route, link, handleClick } = props;
   const handlePress = () => {
     console.log('pressed')
   }
 
   return (
     <View style={styles.flexItem}>
-      <Pressable onPress={handlePress}>
-        <Link to={route}>
+      <Pressable onPress={handleClick ? handleClick : handlePress}>
+        {link ? <Link to={route}>
           <Text style={styles.text}>{tab}</Text>
-        </Link>
+        </Link> : <Text style={styles.text}>{tab}</Text>}
       </Pressable>
     </View>
     )
 }
 
 const AppBar = () => {
+  const { data } = useQuery(GET_ME, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const authStorage = useAuthStorage()
+  const apolloClient = useApolloClient();
+
+
+  const logout = () => {
+    authStorage.removeAccessToken().then(() => {
+      apolloClient.resetStore();
+    })
+  }
 
   return (
     <View style={styles.container}>
       <ScrollView horizontal style={styles.scrollView}>
-        <AppBarTab tab={'Repositories'} route={"/"}/>
-        <AppBarTab tab={'Sign In'} route={"/sign-in"}/>
+        <AppBarTab tab={'Repositories'} route={"/"} link={true}/>
+        {data?.me ?  <AppBarTab tab={'Sign Out'} handleClick={logout}/> : 
+         <AppBarTab tab={'Sign In'} route={"/sign-in"} link={true}/>}
       </ScrollView>
     </View>)
 };
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,8 @@ import theme from '../theme';
 import Text from './Text';
 import FormikTextInput from './FormikTextInput';
 import * as Yup from 'yup';
+import { useNavigate } from "react-router-dom";
+import useSignIn from '../hooks/useSignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -47,6 +49,8 @@ const SignInForm = ({ onSubmit }) => {
   )
 }
 const SignIn = () => {
+  const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const SignInSchema = Yup.object().shape({
     username: Yup.string()
@@ -55,10 +59,17 @@ const SignIn = () => {
       .required('Password is required')
   })
 
-  const onSubmit = (values) => {
-    console.log('submit')
-    console.log(values)
-  }
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+    try {
+      const res = await signIn({ username, password });
+      if (res) {
+        navigate("/")
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
   const initialValues = {
     username: '',
     password: '',
