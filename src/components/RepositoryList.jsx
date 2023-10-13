a99f94af95008df81a5df15d851854922151dac8
Eloagbawe <eloagbawe@gmail.com>
2023-10-13 23:09:56 +0100

    update the repositorylist container, implement sort and filter repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,75 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from "react-router-native";
-import { Button, Menu } from 'react-native-paper';
+import { Button, Menu, Searchbar } from 'react-native-paper';
+import { useDebounce } from 'use-debounce';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
-  menuItem: {
-    color: '#000000'
-  },
-  button: {
-    color: '#000000'
+  flex: {
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 });
 
-// const repositories = [
-//   {
-//     id: 'jaredpalmer.formik',
-//     fullName: 'jaredpalmer/formik',
-//     description: 'Build forms in React, without the tears',
-//     language: 'TypeScript',
-//     forksCount: 1589,
-//     stargazersCount: 21553,
-//     ratingAverage: 88,
-//     reviewCount: 4,
-//     ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
-//   },
-//   {
-//     id: 'rails.rails',
-//     fullName: 'rails/rails',
-//     description: 'Ruby on Rails',
-//     language: 'Ruby',
-//     forksCount: 18349,
-//     stargazersCount: 45377,
-//     ratingAverage: 100,
-//     reviewCount: 2,
-//     ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
-//   },
-//   {
-//     id: 'django.django',
-//     fullName: 'django/django',
-//     description: 'The Web framework for perfectionists with deadlines.',
-//     language: 'Python',
-//     forksCount: 21015,
-//     stargazersCount: 48496,
-//     ratingAverage: 73,
-//     reviewCount: 5,
-//     ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
-//   },
-//   {
-//     id: 'reduxjs.redux',
-//     fullName: 'reduxjs/redux',
-//     description: 'Predictable state container for JavaScript apps',
-//     language: 'TypeScript',
-//     forksCount: 13902,
-//     stargazersCount: 52869,
-//     ratingAverage: 0,
-//     reviewCount: 0,
-//     ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
-//   },
-// ];
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const SortRepositoryMenu = () => {
+const RepositoryListHeader = ({handleSort, sortName, onChangeSearch, searchQuery}) => {
   const [visible, setVisible] = useState(false);
-  const [selected, setSelected] = useState('Latest repositories')
 
   const openMenu = () => setVisible(true);
 
@@ -77,62 +30,131 @@ const SortRepositoryMenu = () => {
 
   
   const handleChange = (title) => {
-    setSelected(title)
+    handleSort(title)
     closeMenu()
   }
 
   return (
-    <View
-      style={{
+    <View style={{backgroundColor:'#E1E5E7'}}>
+      <View style={{marginTop: 20, marginBottom: 5, marginHorizontal: 20}}>
+        <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{borderRadius: 10}}
+        theme={{ colors: { elevation: {level3: '#ffffff'} } }}/>
+      </View>
+
+      <View
+        style={{
         paddingVertical: 5,
         flexDirection: 'row',
+        marginHorizontal: 20,
+        marginVertical: 10,
       }}>
-      <Menu
-        contentStyle={{
-          backgroundColor: '#ffffff',
-        }}
-        visible={visible}
-        onDismiss={closeMenu}
-        anchor={<Button onPress={openMenu}>Sort By - {selected}</Button>}>
-        <Menu.Item onPress={() => handleChange('Latest repositories')} title="Latest repositories" />
-        <Menu.Item onPress={() => handleChange('Highest rated repositories')} title="Highest rated repositories" />
-        <Menu.Item  onPress={() => handleChange('Lowest rated repositories')} title="Lowest rated repositories" />
-      </Menu>
+        
+        <Menu
+          contentStyle={{
+            backgroundColor: '#ffffff',
+          }}
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={<View style={styles.flex}><Button onPress={openMenu}>{sortName}</Button>
+          <MaterialCommunityIcons name="menu-down" size={26} color="#1C1E21" onPress={openMenu}
+          /></View>}>
+          <Menu.Item onPress={() => handleChange('Latest repositories')} title="Latest repositories" />
+          <Menu.Item onPress={() => handleChange('Highest rated repositories')} title="Highest rated repositories" />
+          <Menu.Item  onPress={() => handleChange('Lowest rated repositories')} title="Lowest rated repositories" />
+        </Menu>
+      </View>
     </View>
   )
 }
 
-export const RepositoryListContainer = ({ repositories }) => {  
-  const navigate = useNavigate()
+export class RepositoryListContainer extends React.Component {
+  
+  renderHeader = () => {
+    // this.props contains the component's props
+    const props = this.props;
+    const { handleSort, sortName, onChangeSearch, searchQuery } = props
 
-  // Get the nodes from the edges array
-  const repositoryNodes = repositories
+    return (
+      <RepositoryListHeader
+      handleSort={handleSort} sortName={sortName}
+      onChangeSearch={onChangeSearch} searchQuery={searchQuery}
+      />
+    );
+  };
+
+  render() {
+    const {repositories, navigate} = this.props;
+  
+    const repositoryNodes = repositories
     ? repositories?.edges?.map(edge => edge.node)
     : [];
 
- 
-
-  return (
-    <FlatList
-      ListHeaderComponent={() => <SortRepositoryMenu/>}
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({item}) =>
-      <Pressable onPress={() => navigate(`/repositories/${item.id}`)}>
-        <RepositoryItem repositoryData={item}/>
-      </Pressable>
-      }
-    />
-  );
-};
+    return (
+      <FlatList
+        // ...
+        ListHeaderComponent={this.renderHeader}
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({item}) =>
+        <Pressable onPress={() => navigate(`/repositories/${item.id}`)}>
+          <RepositoryItem repositoryData={item}/>
+        </Pressable>}
+      />
+    );
+  }
+}
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const navigate = useNavigate();
+  const [sortBy, setSortBy] = useState({
+    orderBy: 'CREATED_AT',
+    orderDirection: 'DESC',
+    name: 'Latest repositories'
+  })
+  const [filter, setfilter] = useState('')
+  const [value] = useDebounce(filter, 500);
+
+  const { repositories } = useRepositories({orderBy: sortBy.orderBy, orderDirection: sortBy.orderDirection,
+    searchKeyword: value
+  });
+
+  const handleSort = (sort) => {
+    switch(sort) {
+      case 'Highest rated repositories':
+        setSortBy({
+          orderBy: 'RATING_AVERAGE',
+          orderDirection: 'DESC',
+          name: 'Highest rated repositories'
+        })
+        break;
+      case 'Lowest rated repositories':
+        setSortBy({
+          orderBy: 'RATING_AVERAGE',
+          orderDirection: 'ASC',
+          name: 'Lowest rated repositories'
+        })
+        break;
+      default:
+        setSortBy({
+          orderBy: 'CREATED_AT',
+          orderDirection: 'DESC',
+          name: 'Latest repositories'
+        })
+        break;
+    }
+  }
+
+  const onChangeSearch = (value) => {
+    setfilter(value)
+  }
   
   return (
-    
-    <RepositoryListContainer repositories={repositories}/>
-
+    <RepositoryListContainer repositories={repositories} handleSort={handleSort} sortName={sortBy.name}
+    searchQuery={filter} onChangeSearch={onChangeSearch} navigate={navigate}/>
   )
 
 }
